826e2461af7a9e14da641e46a9c621a017ae4824
Thilen Lama <thilenspurs@gmail.com>
2024-06-23 19:48:15 -0400

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Thilen Lama on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
